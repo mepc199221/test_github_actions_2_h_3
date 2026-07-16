e07a521a039c3de1ed9f506002e78d8b8cae11fa
Miguel Pérez <mepc199221@gmail.com>
2026-07-15 21:25:10 -0400

    feat(update)

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hack_github-_actions_2_h_3/'
+  base: '/test_github_actions_2_h_3/'
 })
